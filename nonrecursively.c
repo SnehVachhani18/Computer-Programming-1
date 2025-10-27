fe043a1e2d8a883fb305bb3151342f6d81940e29
SnehVachhani18 <vachhanisnehm@gmail.com>
2025-10-27 14:43:44 +0530

    Add files via upload

---
+++ nonrecursively.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+
+    int sumofno(int);
+    printf("%d",sumofno(3));
+
+
+}
+int sumofno(int n)
+{
+    int s=0;
+    for(int i=1;i<=n;i++)
+            s=s+i;
+    return s;
+
+}
